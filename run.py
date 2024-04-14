e7553f0296cab3f33a01f9bbd45b0f68c6f4ee2c
bcross98 <112006189+bcross98@users.noreply.github.com>
2024-04-14 13:22:08 -0500

    comments

--- run.py
+++ run.py
@@ -1,13 +1,4 @@
-#Welcome to Python. Python is a computer language that's highly readable and very easy to pick up, with a bit of practice.
-#
 #Imports
-#This tells Python what Libraries you want to use.
-#Think of Libraries like tool kits.
-#Pyautogui lets you control mouse clicks/keyboard clicks
-#Keyboard lets you control keyboard clicks and it can listen for a specific key to be clicked
-#Threading lets you have multiple processes running at the same time. Normally you only get defaulted 1, this lets you run two things at the same time.
-#Time has a function called sleep, this is essentially a pause function.
-#OS lets you control things about the Operating System. In this case we're using it to exit the program with os._exit(1)
 import pyautogui
 import keyboard
 from threading import Thread
@@ -15,33 +6,8 @@ import time
 import os
 
 #Functions
-#These are like making drawers for your tools to go in. They help keep things nice and tidy plus you can test things easier.
-#Def is the keyword to let the computer know you're using a function. Don't ask me why it's like this.
 #
-#os.system('cls||clear') clears the command prompt window of any text. I use this to keep it looking pretty.
 #
-#print is pretty obvious. It displays text in the command prompt window.
-#
-#time.sleep(10) is a timeout function. This tells the program to wait before doing anything else.
-#
-#pyautogui.mouseDown() tells the program to press down the left mouse button (left is default, you'll have to specify if you want a different result)
-#
-#While True: is a loop that you can use in Python. It will run the code inside of it (everything indented) as long as it stays True. Break is the keyword to get out of it, usually you set up and if statement to look for a way to stop the loop or else it runs forever. You can also add elif to your if statement, this just means "else if" think of it as a second way to stop or check for something.
-#
-#EX.
-#While JerettIsGay:
-#   He sucks dick
-#
-#   if JerettIsGay == False:
-#       break
-#   elif JerettIsBi == True:
-#       break
-#
-#input is used to get something from the user. In this program, it asks how many iron picks you have and stores it in a value called 'picks'. You can name values almost anything you want, as long as it doesn't exist in a library you're using. You'll know if a library is using that word because it's blue.
-#
-#When you get something from the user, it's stored in a 'string' which is a fancy way of saying the computer is reading it like a word. Sometimes you have to change it from a word to a number (integer). To do that you'll use something like int(picks) to turn it into a number or str(picks) to turn it into a "word".
-#
-#keyboard.is_pressed('e') is really simple to understand. It's essentially paying attention to if the user presses the 'e' key
 #
 #Simple Cobblestone Function
 #Runs infinitely
@@ -98,9 +64,7 @@ def XPFarm():
         time.sleep(0.5)
 
 #These functions set up a second thread listening for the escape key
-#todo: make it smaller
-#
-#Don't worry about any of these. I just recently learned this, I don't know if I can properly explain it in a way that'll make sense.
+#todo: make it more compact
 def SimpleCobblestoneListener():
     t = Thread(target=SimpleCobblestoneGenerator)
     t.daemon = True
@@ -126,10 +90,3 @@ def XpFarmListener():
     while True:
         if keyboard.is_pressed('e'):
             os._exit(1)
-
-#Start Program
-#
-#I'm calling the ComplexCobblestoneListener, this will run that program that also runs the ComplexCobblestoneGenerator function.
-#This is the only function I'm calling, this is the one that needs the most testing and work.
-#Later I'm going to connect the ui to this script and the UI will start the program.
-ComplexCobblestoneListener()
