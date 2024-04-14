ff74c1d0f0093dc177ae881b0c824dc3609eaa3e
bcross98 <112006189+bcross98@users.noreply.github.com>
2024-04-14 11:48:44 -0500

    Lots of comments and separated ui

---
+++ ui.py
@@ -0,0 +1,35 @@
+#GUI
+#
+#
+#
+import tkinter as tk
+
+def mainWindow():
+    #Window
+    window = tk.Tk()
+    window.title('MC Helper')
+
+    #Simple Cobblestone Button
+    #todo: link to function
+    simpleButton = tk.Button(window, text="Simple Cobble afk",)
+    simpleButton.grid(row=1, column=0)
+
+    #Complex Cobblestone Button
+    #todo: link to function
+    complexButton = tk.Button(window, text="Complex Cobble afk",)
+    complexButton.grid(row=1, column=1)
+
+    #XP Farm Button
+    #todo: link to function
+    xpFarmButton = tk.Button(window, text='XP Farm afk',)
+    xpFarmButton.grid(row=1, column=2)
+
+    #Exit Button
+    simpleButton = tk.Button(window, text='Exit', command=window.destroy)
+    simpleButton.grid(row=2, column=1)
+
+    #Starts program
+    window.mainloop()
+
+#Start Program
+mainWindow()
